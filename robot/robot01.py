ddf6c900e0b563c48bf63120d024bf55d65e9160
n3r0nq <nerofsg@gmail.com>
2019-01-29 16:36:23 +0100

    roboty

---
+++ robot/robot01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rg
+
+
+class Robot:
+
+    def act(self, game):
+        # print(self.location)
+        # print(rg.CENTER_POINT)
+        # print(game)
+        x, y = self.location
+        loc2 = (x - 1, y)
+        print(self.location, rg.loc_types(self.location))
+        print(loc2, rg.loc_types(self.location))
+        print(rg.locs_around(self.location, filter_out=('invalid', 'obstacle')))
+
+        if self.location != rg.CENTER_POINT:
+            return['move', rg.toward(self.location, rg.CENTER_POINT)]
+
+        return['guard']
